f79a000026c2e3d03a84c77b71bf1e6e54dc4f3f
Dmitrii Torkhov <dmitriytorkhov@gmail.com>
2021-08-19 11:12:14 +0300

    add simple test

---
+++ tests/test_simple.cpp
@@ -0,0 +1,35 @@
+//
+//  test_simple.cpp
+//  maw
+//
+//  Created by Dmitrii Torkhov <dmitriitorkhov@gmail.com> on 16.08.2021.
+//  Copyright © 2021 Dmitrii Torkhov. All rights reserved.
+//
+
+#include <iostream>
+
+#include <maw/maw.h>
+
+int main() {
+    oo::maw audio{};
+
+    std::cout << "initialized...";
+    std::getchar();
+
+    audio.load("sample.mp3");
+
+    std::cout << "loaded...";
+    std::getchar();
+
+    audio.play("sample.mp3");
+
+    std::cout << "playing...";
+    std::getchar();
+
+    audio.stop("sample.mp3");
+
+    std::cout << "stopped...";
+    std::getchar();
+
+    return 0;
+}
